8e598d38c7d5402f4a1ee4496485ddde056623ba
deepsnow <doug.axtell@gmail.com>
2015-12-01 16:59:57 -0500

    I can't get my protractor tests to work - I can drive them, but I can't verify results

--- Practical/protractor/todomvc_angular_conf.js
+++ Practical/protractor/todomvc_angular_conf.js
@@ -1,5 +1,10 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['todomvc_angular_simple_ops_tests.js'],
-  framework: 'jasmine2'
+  //framework: 'jasmine2',
+  
+/*   onPrepare: function() {
+	browser.ignoreSynchronization = true
+    browser.driver.get('http://todomvc.com/examples/angular2/')  
+  }, */
 };
--- Practical/protractor/todomvc_angular_simple_ops_tests.js
+++ Practical/protractor/todomvc_angular_simple_ops_tests.js
@@ -1,15 +1,26 @@
 describe('todomvc angular2 tests', function() {
-  it('add one to-do', function() {
-	browser.ignoreSynchronization = true
-    browser.get('http://todomvc.com/examples/angular2/')
+  it('add to-dos and verify that they are displayed', function() {
 
-    var el = element(by.binding('newtodo'))
-	el.clear()
-	el.sendKeys('task 1\r')
-	el.sendKeys('task 2\r')
-
-    var resultList = element.all(by.repeater('todo of todoStore.todos'))
-    expect(resultList.count()).toEqual(2)
+	browser.ignoreSynchronization = true; // The site/app under test uses AngularJS in what might be called a manually bootstrapped fashion, i.e. without use the ng-app directive.
+    browser.get('http://todomvc.com/examples/angular2/');
+	
+	browser.driver.sleep(1000); // There's got to be a better way to wait for the custom <todo-app></todo-app> tag (which is a custom AngularJS directive) to load.
+	
+	var el = element(by.css('input.new-todo'));
+	el.clear();
+	el.sendKeys('task 1\r\n');
+	el.sendKeys('task 2\r\n');
+	
+	browser.driver.sleep(2000); // aid in debugging
+	
+    var resultlist = element.all(by.repeater('todo in todoStore.todos')); // This doesn't work...
+    expect(resultlist.count()).toEqual(2);
+	
+    //These don't work either
+	// var liFisrt = element(by.css('ul.todo-list > li:nth-child(2) > div > label'));
+    // expect(liFirst.getText().toMatch('task 1'));
+    // var liSecond = element(by.css('ul.todo-list > li:nth-child(3) > div > label'));
+    // expect(liSecond.getText().toMatch('task 2'));
 	
   });
 });
