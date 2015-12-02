634dab41e2eacb7d20a10a9124c8758329356e47
deepsnow <doug.axtell@gmail.com>
2015-12-02 07:24:08 -0500

    in the middle of implementing edit-todos test and want to preserve the most recent cleanup I've achieved while getting to this point

--- Practical/nightwatch/basic_tests.js
+++ Practical/nightwatch/basic_tests.js
@@ -1,60 +1,71 @@
 module.exports = {
-  
+
 	'create todos' : function (client) {
 		client
 			.url('http://todomvc.com/examples/angular2/')
-			.waitForElementVisible('input.new-todo', 1000)
+			.waitForElementVisible('input.new-todo', 2000) // I've been able to get away with a wait time of 1000 ms for most of my testing. But, I just did a test run where that interval proved too short.
 			.assert.visible('input.new-todo');
 		createToDos(client, [ 'task A', 'task B' ]);
-		client
-			.assert.visible('ul.todo-list')
-			.assert.containsText('ul.todo-list > li:nth-child(2) > div > label', 'task A')
-			.assert.containsText('ul.todo-list > li:nth-child(3) > div > label', 'task B')
+		client.assert.visible('ul.todo-list');
+		verifyNthToDoText(client, '2', 'task A');
+		verifyNthToDoText(client, '3', 'task B');
 	},
-  
+
 	'complete todo' : function (client) {
-		client.expect.element('ul.todo-list > li:nth-child(3) > div > input').to.not.be.selected;
-		client
-			.click('ul.todo-list > li:nth-child(3) > div > input', function (result) {
-				client.assert.equal(typeof result, "object");
-				client.assert.equal(result.status, 0);
-			})
-		.expect.element('ul.todo-list > li:nth-child(3) > div > input').to.be.selected;
+		var cssSelector = 'ul.todo-list > li:nth-child(3) > div > input';
+		client.expect.element(cssSelector).to.not.be.selected;
+		markNthToDoCompleted(client, '3');
+		client.expect.element(cssSelector).to.be.selected;
 	},
-  
+
 	'delete todos' : function (client) {
-		deleteNthListItem(client, '3'); // this one is already marked as completed
+		deleteNthToDo(client, '3'); // this one is already marked as completed
 		client.assert.elementNotPresent('ul.todo-list > li:nth-child(3)')
-		deleteNthListItem(client, '2'); // this one is not yet completed
+		deleteNthToDo(client, '2'); // this one is not yet completed
 		client.assert.elementNotPresent('ul.todo-list > li:nth-child(2)')
+	},
+
+	'edit todos' : function (client) {
+		createToDos(client, [ 'task C', 'task D' ]);
+		verifyNthToDoText(client, '2', 'task C');
+		verifyNthToDoText(client, '3', 'task D');
+		markNthToDoCompleted(client, '2');
+		client
+			.pause(4000)
 			.end();
-  },
-  
-  // 'edit todos' : function (client) {
-	// createToDos(client, [ 'task C', 'task D' ]);  
-    // client
-	  // .assert.containsText('ul.todo-list > li:nth-child(2) > div > label', 'task D')
-      // .end();
-  // },
-  
+	},
+
 };
 
 function createToDos (client, todoDescriptions) {
 	for (i = 0; i < todoDescriptions.length; i++) {
-		client
-		  .setValue('input.new-todo', todoDescriptions[i] + '\r\n')
+		client.setValue('input.new-todo', todoDescriptions[i] + '\r\n')
 	}
 }
 
-function deleteNthListItem (client, itemNum) {
+function deleteNthToDo (client, itemNum) {
 	var cssSelector = 'ul.todo-list > li:nth-child(' + itemNum + ') > div > input';
 	client.moveToElement(cssSelector, 10, 10, function (result) {
-		this.assert.equal(typeof result, "object");
-		this.assert.equal(result.status, 0);
+		client.assert.equal(typeof result, "object");
+		client.assert.equal(result.status, 0);
 	});
 	cssSelector = 'ul.todo-list > li:nth-child(' + itemNum + ') > div > button';
 	client.click(cssSelector, function (result) {
 		client.assert.equal(typeof result, "object");
 		client.assert.equal(result.status, 0);
 	});
+}
+
+function verifyNthToDoText(client, itemNum, desc) {
+	var cssSelector = 'ul.todo-list > li:nth-child(' + itemNum + ') > div > label';
+	client.assert.containsText(cssSelector, desc);
+}
+
+function markNthToDoCompleted(client, itemNum) {
+	var cssSelector = 'ul.todo-list > li:nth-child(' + itemNum + ') > div > input';
+	client
+		.click(cssSelector, function (result) {
+			client.assert.equal(typeof result, "object");
+			client.assert.equal(result.status, 0);
+		});
 }
